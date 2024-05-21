eef2af5b942e2d92a94ed0eaa3c3547289ebf05f
elmurat-karabekov <karabekov.elmurat@yandex.kz>
2024-05-21 20:19:00 +0500

    webpack config files created and removed link & script tags from index.hmtml

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { initialCards } from "./scripts/cards";
+
 const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
 
