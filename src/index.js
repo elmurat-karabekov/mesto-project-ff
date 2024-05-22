6095cca74ff5c026bff332d826c3b0f907968ca8
elmurat-karabekov <karabekov.elmurat@yandex.kz>
2024-05-22 22:37:37 +0500

    open/close popups logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './pages/index.css';
 import { initialCards } from "./scripts/cards";
 import { createCardElement, deleteCard } from './scripts/card';
+import { openModal, closeModal } from './scripts/modal';
 
 // В файле index.js должны остаться:
 // объявления и инициализация глобальных констант и переменных с DOM-элементами страницы,
@@ -9,7 +10,32 @@ import { createCardElement, deleteCard } from './scripts/card';
 
 const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
+const editProfileBtn = document.querySelector('.profile__edit-button');
+const editProfilePopup = document.querySelector('.popup_type_edit');
+const pagePopups = document.querySelectorAll('.popup');
+const addCardButton = document.querySelector('.profile__add-button');
+const addCardPopup = document.querySelector('.popup_type_new-card');
 
 initialCards.forEach((card) =>
   placesList.append(createCardElement(cardTemplate, card, deleteCard))
 );
+
+editProfileBtn.addEventListener('click', () => {
+  openModal(editProfilePopup);
+})
+
+addCardButton.addEventListener('click', () => {
+  openModal(addCardPopup);
+})
+
+pagePopups.forEach(popup => {
+  popup.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('popup_is-opened')) {
+      closeModal(popup);
+    }
+    if (evt.target.classList.contains('popup__close')) {
+      closeModal(popup);
+    }
+  })
+})
+
--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,7 +1,18 @@
-export function openModal() {
+export function openModal(modal) {
+  modal.classList.add('popup_is-opened');
 
+  document.addEventListener('keydown', closeModalOnESC)
 }
 
-export function closModal() {
+export function closeModal(modal) {
+  modal.classList.remove('popup_is-opened');
 
+  document.removeEventListener('keydown', closeModalOnESC);
+}
+
+function closeModalOnESC(evt) {
+  if (evt.key === 'Escape') {
+    const openedPopup = document.querySelector('.popup_is-opened');
+    closeModal(openedPopup);
+  }
 }
